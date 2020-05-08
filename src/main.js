e2f70fe08ea8917873ce5458207dfbc4ea388a35
Bartosz <bartoszurbanski.biznes@gmail.com>
2020-05-08 13:40:46 +0200

    First testing deploy

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import experience from './js/experience';
-import toggleNavigation from './js/navigation';
-import './scss/style.scss';
-
-experience('experience', [['2020, 1, 18']]);
-
-const burger = document.querySelector('.burger');
-
-burger.addEventListener('click', toggleNavigation);
